636d87ede59542a44370d3c3ee9e29f2bf160cee
Matthieu Vachon <matt@dfuse.io>
2021-02-02 16:53:42 -0500

    Fixed x-forwarded-address extracted from dgrpc

--- server.go
+++ server.go
@@ -805,16 +805,36 @@ func validateAuth(ctx context.Context, check AuthCheckerFunc) (context.Context, 
 	return authCtx, err
 }
 
-var portSuffixRegex = regexp.MustCompile(":[0-9]{2,5}$")
+var portSuffixRegex = regexp.MustCompile(`:[0-9]{2,5}$`)
 
 func extractGRPCRealIP(ctx context.Context, md metadata.MD) string {
-	xff := md.Get("x-forwarded-for")
-	if len(xff) > 0 {
-		// The previous code was getting the element at index `len(xff) - 2`, frankly, I have
-		// no idea why, it makes no sense unless the value is exactly 2, otherwise as it grows,
-		// the ip address picked up will be in the middle of the slice. It should probably be
-		// either the first element 0 or the last one. I chose the to pick the last one.
-		return strings.TrimSpace(xff[len(xff)-1])
+	xForwardedFor := md.Get("x-forwarded-for")
+	if len(xForwardedFor) > 0 {
+		// When behind a Google Load Balancer, the only two values that we can
+		// be sure about are the `n - 2` and `n - 1` (so the last two values
+		// in the array). The very last value (`n - 1`) is the Google IP and the
+		// `n - 2` value is the actual remote IP that reached the load balancer.
+		//
+		// When there is more than 2 IPs, all other values prior `n - 2` are
+		// those coming from the `X-Forwarded-For` HTTP header received by the load
+		// balancer directly, so something a client might have added manually. Since
+		// they are coming from an HTTP header and not from Google directly, they
+		// can be forged and cannot be trusted.
+		//
+		// Ideally, to trust the received IP, we should validate it's an actual
+		// query coming from Netlify. For now, we are very lenient and trust
+		// anything that comes in and looks like an IP.
+		//
+		// @see https://cloud.google.com/load-balancing/docs/https#x-forwarded-for_header
+		if len(xForwardedFor) == 2 {
+			return strings.TrimSpace(xForwardedFor[0])
+		}
+
+		// There is more than 2 addresses, only the element at `n - 2` should be
+		// considered, all others cannot be trusted (assuming we got `[a, b, c, d]``,
+		// we want to pick element `c` which is at index 2 here so `len(elements) - 2`
+		// gives the correct value)
+		return strings.TrimSpace(xForwardedFor[len(xForwardedFor)-2])
 	}
 
 	if peer, ok := peer.FromContext(ctx); ok {
