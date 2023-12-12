7a52eccb58911459878f78bf6bcbdace11ede8f3
Matthieu Vachon <matt@streamingfast.io>
2023-12-12 13:58:33 -0500

    Added documentation on `Server` interface `Launch` method

--- server/interface.go
+++ server/interface.go
@@ -8,6 +8,10 @@ import (
 
 type Server interface {
 	RegisterService(f func(gs grpc.ServiceRegistrar))
+
+	// Launch the server and listen on the given address, this is a blocking call and
+	// will block the current goroutine until the server is terminated by a call to
+	// Shutdown() or an error occurs (e.g. the server fails to start).
 	Launch(serverListenerAddress string)
 
 	ServiceRegistrar() grpc.ServiceRegistrar
